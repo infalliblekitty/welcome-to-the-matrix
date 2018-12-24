83b4cb17307b583bc0d2fe0ae0eee2ce775819ff
infalliblekitty <45752341+infalliblekitty@users.noreply.github.com>
2018-12-24 22:26:14 +0800

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+a1 = raw_input('Please type an adjective and hit enter:\r\n')
+b1 = raw_input('Please type an adjective and hit enter:\r\n')
+c1 = raw_input('Please type a type of bird and hit enter:\r\n')
+d1 = raw_input('Please type a room in a house and hit enter:\r\n')
+p1 = raw_input('Please type a verb (past tense) and hit enter:\r\n')
+f1 = raw_input('Please type a verb and hit enter:\r\n')
+g1 = raw_input('Please type a name and hit enter:\r\n')
+h1 = raw_input('Please type a noun and hit enter:\r\n')
+i1 = raw_input('Please type a liquid and hit enter:\r\n')
+j1 = raw_input('Please type a verb ending in -ing and hit enter:\r\n')
+k1 = raw_input('Please type a part of the body (plural) and hit enter:\r\n')
+l1 = raw_input('Please type a plural noun and hit enter:\r\n')
+m1 = raw_input('Please type a verb ending in -ing and hit enter:\r\n')
+n1 = raw_input('Please type a noun and hit enter:\r\n')
+
+z = """It was a %s, cold November day. I woke up to the %s smell of %s roasting
+in the %s downstairs. I %s down the stairs to see if I could help %s the dinner.
+My mom said, See if %s needs a fresh %s. So I carried a tray of %s into the
+%s room. When I got there, I could not believe my %s. There were %s %s on
+the %s.
+""" %(a1, b1, c1, d1, p1, f1, g1, h1, i1, j1, k1, l1, m1, n1)
+print z
